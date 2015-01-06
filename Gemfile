862cc0a28685abaf0915889656466b6cb885014d
kadarme <kn.adarme@gmail.com>
2015-01-06 22:15:39 +0000

    Integrated components needed for uploading.

--- Gemfile
+++ Gemfile
@@ -58,3 +58,9 @@ gem 'figaro', '1.0'
 gem 'pundit'
 
 gem 'redcarpet'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
+
+gem 'fog'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
   has_many :posts
+  mount_uploader :avatar, AvatarUploader
   
   def admin?
     role == 'admin'
