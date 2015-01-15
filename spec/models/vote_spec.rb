2cb4c4330e9fa416a29566e10157c54a2fd52d3d
kadarme <kn.adarme@gmail.com>
2015-01-15 05:44:15 +0000

    Added vote function.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,15 +1,17 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         v = Vote.new(value: 1)
-        expect( v.valid? ).to_eq(true)
+        expect( v.valid? ).to eq(true)
         
         v2 = Vote.new(value: -1)
-        expect( v2.valid? ).to_eq(true)
+        expect( v2.valid? ).to eq(true)
         
         v3 = Vote.new(value: 2)
-        expect( v3.valid? ).to_eq(false)
+        expect( v3.valid? ).to eq(false)
       end
     end
   end 
