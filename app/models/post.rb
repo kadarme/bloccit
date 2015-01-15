6d8430f79883d7837e6db47f1abd5dd610deccfd
kadarme <kn.adarme@gmail.com>
2015-01-15 19:28:38 +0000

    Mocked with RSpec.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
+ 
+  default_scope { order('rank DESC') }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
   
   mount_uploader :image, ImageUploader
   
@@ -24,17 +31,6 @@ class Post < ActiveRecord::Base
     
     update_attribute(:rank, new_rank)
   end
-  
-  after_create :create_vote
-  
-  default_scope { order('rank DESC') }
-
-  validates :title, length: { minimum: 5 }, presence: true
-  validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
-  
-  private
   
   def create_vote
     user.votes.create(value: 1, post: self)
