2bca81f2ff327f83f5f1f90ebd5da00d77f20ec6
kadarme <kn.adarme@gmail.com>
2015-01-22 06:31:01 +0000

    Deleted with Ajax request.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -20,10 +22,12 @@ class CommentsController < ApplicationController
     
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error deleting the comment. Please try again."
-      redirect_to [@post.topic, @post]
+    end
+    
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
